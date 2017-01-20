472f3feb7a3c8809f2b5919e4590ec075604c0d5
dark32167 <dark32167@yandex.ru>
2017-01-20 03:29:30 +0300

    end

---
+++ kursMMHTP/Model/MathModel.cs
@@ -0,0 +1,120 @@
+﻿using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kursMMHTP.Model
+{
+    class MathModel
+    {
+        #region var
+        ViewModel.MainWindowModel MainViewModel = ViewModel.MainWindowModel.getInsance();
+
+        //объём суспензии в фильтре
+        private double Vf;
+        //поверхность фильтрования, м2;
+        private double S;
+        //объемная доля твердой фазы в суспензии (от 0 до 1)
+        private double x;
+        //высоту слоя осадка (см)
+        private double hoc;
+        //разность давлений, н• м-2;
+        private double dP;
+        //вязкость жидкой фазы суспензии, н•сек• м-2;
+        private double mu;
+        //сопротивление фильтровальной перегородки, м-1
+        private double Rfp;
+        //сопротивление слоя осадка, м-1
+        private double Roc;
+        //удельное объёмное сопротивление осадка (сопротивление, оказываемое потоку фильтрата равномерным слоем осадка толщиной 1 м), м-2.
+        private double r0;
+        //объёмный расход суспензии через фильтрующую перегородку, м3/c; 
+        private double Gout;
+        //время фильтрования
+        private double t;
+        //объём фильтрата в сборнике
+        private double Vc;
+        //точки для граффика Vf
+        private List<DataPoint> PointsVf = new List<DataPoint>();
+        //точки для граффика Vf
+        private List<DataPoint> PointsVc = new List<DataPoint>();
+        //точки для граффика Vf
+        private List<DataPoint> PointsHoc = new List<DataPoint>();
+        #endregion
+
+        public MathModel()
+        {
+            Vf = MainViewModel.Vf;
+            S = MainViewModel.S;
+            x = MainViewModel.x;
+            dP = MainViewModel.dP;
+            mu = MainViewModel.Mu;
+            Rfp = MainViewModel.Rfp * 10000000000;
+            r0 = MainViewModel.r0 * 1000000000000;
+            Calculation();
+        }
+
+        private void Calculation()
+        {
+
+            hoc = HocCalc();
+            Roc = RocCalc();
+            Gout = CalcGout();
+            t = CalcT();
+
+            Vc = CalcVc();
+            MainViewModel.Vc = Vc;
+            MainViewModel.t = t;
+
+            //рассчет данных для графика
+            for (double i = 0.1; i < t && i < 10000; i++)
+            {
+                double graphVc = Gout * i * S;
+                double graphVf = Vf - graphVc;
+                double graphHoc = (x * graphVc) / S;
+                PointsVf.Add(new DataPoint(i, graphVf));
+                PointsVc.Add(new DataPoint(i, graphVc));
+                PointsHoc.Add(new DataPoint(i, graphHoc));
+            }
+
+            MainViewModel.PointsVf = PointsVf;
+            MainViewModel.PointsVc = PointsVc;
+            MainViewModel.PointsHoc = PointsHoc;
+            MainViewModel.hoc = hoc * 100;
+            MainViewModel.Vfout= CalcVf();
+
+        }
+
+        private Double CalcVc()
+        {
+            return Gout * S * t;
+        }
+
+        private Double CalcVf()
+        {
+            return hoc * S;
+        }
+
+        private Double CalcT()
+        {
+            return Vf / (S * Gout);
+        }
+
+        private Double HocCalc()
+        {
+            return (x * Vf) / S;
+        }
+
+        private Double RocCalc()
+        {
+            return r0 * hoc;
+        }
+
+        private Double CalcGout()
+        {
+            return dP / (mu * (Rfp + Roc));
+        }
+    }
+}
---
+++ kursMMHTP/ViewModel/MainWindowModel.cs
@@ -0,0 +1,214 @@
+﻿using kursMMHTP.Helpers;
+using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace kursMMHTP.ViewModel
+{
+    class MainWindowModel : DependencyObject
+    {
+        #region singletone
+        private static MainWindowModel instance;
+
+        private MainWindowModel()
+        {
+            Vf = 0.001;
+            x = 0.001;
+            Mu = 0.001;
+            S = 0.001;
+            dP = 400;
+            r0 = 0.001;
+            Rfp = 0.001;
+        }
+
+        public static MainWindowModel getInsance()
+        {
+            if (instance == null)
+            {
+                instance = new MainWindowModel();
+            }
+            return instance;
+        }
+        #endregion
+
+        #region ClickCalculate
+        private ICommand _clickCalculateCommand;
+        public ICommand ClickCalculateCommand
+        {
+            get
+            {
+                return _clickCalculateCommand ?? (_clickCalculateCommand = new CommandHandler(() => ClickCalculateAction(), true));
+            }
+        }
+
+        public void ClickCalculateAction()
+        {
+            new kursMMHTP.Model.MathModel();
+        }
+        #endregion
+
+        #region data from user
+        //объём суспензии в фильтре
+        public Double Vf
+        {
+            get { return (Double)GetValue(VfProperty); }
+            set { SetValue(VfProperty, value); }
+        }
+
+        //объемная доля твердой фазы в суспензии (от 0 до 1)
+        public Double x
+        {
+            get { return (Double)GetValue(xProperty); }
+            set { SetValue(xProperty, value); }
+        }
+
+        //вязкость жидкой фазы суспензии, н•сек• м-2;
+        public Double Mu
+        {
+            get { return (Double)GetValue(MuProperty); }
+            set { SetValue(MuProperty, value); }
+        }
+
+        //поверхность фильтрования, м2;
+        public Double S
+        {
+            get { return (Double)GetValue(SProperty); }
+            set { SetValue(SProperty, value); }
+        }
+        //разность давлений, н• м-2;
+        public int dP
+        {
+            get { return (int)GetValue(dPProperty); }
+            set { SetValue(dPProperty, value); }
+        }
+        //удельное объёмное сопротивление осадка (сопротивление, оказываемое потоку фильтрата равномерным слоем осадка толщиной 1 м), м-2.
+        public Double r0
+        {
+            get { return (Double)GetValue(r0Property); }
+            set { SetValue(r0Property, value); }
+        }
+        //сопротивление фильтровальной перегородки, м-1
+        public Double Rfp
+        {
+            get { return (Double)GetValue(RfpProperty); }
+            set { SetValue(RfpProperty, value); }
+        }
+        #endregion
+
+        #region Data after calc
+        //объём фильтрата в сборнике
+        public Double Vc
+        {
+            get { return (Double)GetValue(VcProperty); }
+            set { SetValue(VcProperty, value); }
+        }
+        //высоту слоя осадка (см)
+        public Double hoc
+        {
+            get { return (Double)GetValue(hocProperty); }
+            set { SetValue(hocProperty, value); }
+        }
+        //время фильтрования
+        public Double t
+        {
+            get { return (Double)GetValue(tProperty); }
+            set { SetValue(tProperty, value); }
+        }
+        //точки для граффика зависимости Vf от t
+        public List<DataPoint> PointsVf
+        {
+            get { return (List<DataPoint>)GetValue(PointsVfProperty); }
+            set { SetValue(PointsVfProperty, value); }
+        }
+        //точки для граффика зависимости Vc от t
+        public List<DataPoint> PointsVc
+        {
+            get { return (List<DataPoint>)GetValue(PointsVcProperty); }
+            set { SetValue(PointsVcProperty, value); }
+        }
+        //точки для граффика зависимости hoc от t
+        public List<DataPoint> PointsHoc
+        {
+            get { return (List<DataPoint>)GetValue(PointsHocProperty); }
+            set { SetValue(PointsHocProperty, value); }
+        }
+        //объем слоя осадка в фильтре
+        public Double Vfout
+        {
+            get { return (Double)GetValue(VfoutProperty); }
+            set { SetValue(VfoutProperty, value); }
+        }
+
+        #endregion
+
+        #region propdp
+
+        // Using a DependencyProperty as the backing store for Vf.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty VfoutProperty =
+            DependencyProperty.Register("Vfout", typeof(Double), typeof(MainWindowModel));
+
+        // Using a DependencyProperty as the backing store for PointsVf.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PointsHocProperty =
+            DependencyProperty.Register("PointsHoc", typeof(List<DataPoint>), typeof(MainWindowModel));
+
+
+        // Using a DependencyProperty as the backing store for PointsVf.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PointsVcProperty =
+            DependencyProperty.Register("PointsVc", typeof(List<DataPoint>), typeof(MainWindowModel));
+
+
+        // Using a DependencyProperty as the backing store for PointsVf.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PointsVfProperty =
+            DependencyProperty.Register("PointsVf", typeof(List<DataPoint>), typeof(MainWindowModel));
+
+        // Using a DependencyProperty as the backing store for t.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty tProperty =
+            DependencyProperty.Register("t", typeof(Double), typeof(MainWindowModel));
+
+        // Using a DependencyProperty as the backing store for hoc.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty hocProperty =
+            DependencyProperty.Register("hoc", typeof(Double), typeof(MainWindowModel));
+
+        // Using a DependencyProperty as the backing store for Vc.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty VcProperty =
+            DependencyProperty.Register("Vc", typeof(Double), typeof(MainWindowModel));
+
+        // Using a DependencyProperty as the backing store for Rfp.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RfpProperty =
+            DependencyProperty.Register("Rfp", typeof(Double), typeof(MainWindowModel));
+
+        // Using a DependencyProperty as the backing store for r0.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty r0Property =
+            DependencyProperty.Register("r0", typeof(Double), typeof(MainWindowModel));
+
+        // Using a DependencyProperty as the backing store for dP.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty dPProperty =
+            DependencyProperty.Register("dP", typeof(int), typeof(MainWindowModel));
+
+        // Using a DependencyProperty as the backing store for S.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SProperty =
+            DependencyProperty.Register("S", typeof(Double), typeof(MainWindowModel));
+
+        // Using a DependencyProperty as the backing store for Mu.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MuProperty =
+            DependencyProperty.Register("Mu", typeof(Double), typeof(MainWindowModel));
+
+        // Using a DependencyProperty as the backing store for x.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty xProperty =
+            DependencyProperty.Register("x", typeof(Double), typeof(MainWindowModel));
+
+        // Using a DependencyProperty as the backing store for Vf.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty VfProperty =
+            DependencyProperty.Register("Vf", typeof(Double), typeof(MainWindowModel));
+
+        #endregion 
+ 
+
+         
+    }
+}
